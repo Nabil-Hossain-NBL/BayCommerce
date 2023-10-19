e5bfa52db218b240e3dd016d6cf47bd4bc631951
Nabil-Hossain-NBL <nabilhossainprf@gmail.com>
2023-10-20 02:02:40 +0600

    Initial setup with router

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="navbar">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Portfolio</a>
+        <a href="#">Contact</a>
+      </div>
+
+      <div>
+        <h1>Welcome to My Website</h1>
+        <p>This is a simple example of an HTML and CSS navbar.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
