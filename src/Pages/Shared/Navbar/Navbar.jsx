d0ed1c0a1dbe55ffefca2f3499904b9196b153d6
Nabil-Hossain-NBL <nabilhossainprf@gmail.com>
2023-10-20 02:31:57 +0600

    Initial navbar

---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <>
+            <div className="navbar">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Services</a>
+                <a href="#">Portfolio</a>
+                <a href="#">Contact</a>
+            </div>
+
+            <div>
+                <h1>Welcome to My Website</h1>
+                <p>This is a simple example of an HTML and CSS navbar.</p>
+            </div>
+        </>
+    );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import Main from './Layout/Main.jsx';
+import Home from './Pages/Home/Home/Home.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
